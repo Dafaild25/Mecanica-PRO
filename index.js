13f5ea981d2da27bd62daf6a7c9f1a3940b609ee
Dafaild25 <154031943+Dafaild25@users.noreply.github.com>
2024-05-18 17:48:48 -0500

    provando js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Atrapar = document.getElementById('formulario-entrada')
 
 Atrapar.addEventListener('submit', e=>{
-    e.preventDefault()
+    e.preventDefault();
     console.log('Enviando')
 })
