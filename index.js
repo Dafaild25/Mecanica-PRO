fe9160f78fe8cf064cff53cd49f88d7b6d93cb5d
Dafaild25 <154031943+Dafaild25@users.noreply.github.com>
2024-05-18 18:44:42 -0500

    funcion agregar

--- index.js
+++
@@ -1,7 +0,0 @@
-const atrapar = document.getElementById('Formulario-entrada');
-atrapar.addEventListener('submit', e => {
-    e.preventDefault();
-
-    const cedula = atrapar['form-cedula'].value;
-    console.log(cedula)
-})
